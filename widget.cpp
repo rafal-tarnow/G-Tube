f2c43175242c8baf7941dc5fcd07e6c2b13f73fa
Rafal Kruszyna <rafal.kruszyna@gmail.com>
2021-01-10 01:34:26 +0100

    first version

--- widget.cpp
+++ widget.cpp
@@ -1,15 +1,79 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QFileDialog>
+#include <QDebug>
+#include <QDir>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+
+    process = new QProcess(this);
+    ui->plainTextEdit->setReadOnly(true);
+    ui->lineEdit->setClearButtonEnabled(true);
+
+
+    connect(process, &QProcess::readyReadStandardOutput, [=](){
+        ui->plainTextEdit->setStyleSheet("QPlainTextEdit {background-color: black; color: green;}");
+        ui->plainTextEdit->appendPlainText(process->readAllStandardOutput()); });
+    connect(process, &QProcess::readyReadStandardError, [=](){
+        ui->plainTextEdit->setStyleSheet("QPlainTextEdit {background-color: black; color: red;}");
+        ui->plainTextEdit->appendPlainText(process->readAllStandardError()); });
+    //connect(process, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished), this, &Widget::close);
+    connect(ui->lineEdit, &QLineEdit::returnPressed, [=](){ process->write(ui->lineEdit->text().toLatin1() + "\n"); ui->lineEdit->clear(); });
+
+}
+
+void Widget::on_pushButton_startDownload_clicked()
+{
+    executeCommand();
+}
+
+void Widget::executeCommand()
+{
+    videoURL = ui->lineEdit_videoURL->text();
+
+    QStringList arguments;
+    arguments << "-o";
+    if(ui->checkBox_autonumber->isChecked())
+    {
+        arguments << (downloadDirecotry + "%(autonumber)s-%(title)s-%(id)s.%(ext)s");
+    }
+    else
+    {
+        arguments << (downloadDirecotry + "%(title)s-%(id)s.%(ext)s");
+    }
+
+    arguments << videoURL;
+
+    process->start("youtube-dl", arguments);
 }
 
 Widget::~Widget()
 {
+    process->close();
+    delete process;
+
     delete ui;
 }
+
+
+
+void Widget::on_pushButton_selectDownloadDir_clicked()
+{
+    QFileDialog dialog;
+    dialog.setFileMode(QFileDialog::DirectoryOnly);
+    dialog.setOption(QFileDialog::ShowDirsOnly, false);
+    dialog.exec();
+
+    QDir dir = dialog.directory();
+    downloadDirecotry = dir.path() + "/";
+    qDebug() << downloadDirecotry << endl;
+
+    ui->label_downloadDir->setText("DOWNLOAD DIR: " + downloadDirecotry);
+}
+
 
--- widget.h
+++ widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QProcess>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -15,7 +16,19 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+
+    void on_pushButton_selectDownloadDir_clicked();
+
+    void on_pushButton_startDownload_clicked();
+
 private:
+    void executeCommand();
+
+private:
+    QString videoURL;
+    QString downloadDirecotry;
+    QProcess *process;
     Ui::Widget *ui;
 };
 #endif // WIDGET_H
